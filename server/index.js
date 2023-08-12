ece6c35cb15ad58a619dcfff04ddf6fa80463c6d
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-08-12 16:05:14 +0000

    Backend for Chat App

--- server/index.js
+++ server/index.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({origin: true}));
 
-const CHAT_ENGINE_PROJECT_ID = "f40226ca-c415-4799-89f9-99206b6c6667";
-const CHAT_ENGINE_PRIVATE_KEY = "26e67981-d8e9-4c06-90b3-122ee25567eb";
+const CHAT_ENGINE_PRIVATE_KEY = "";
 
 app.post("/authenticate", async (req, res) => {
     const { username  } = req.body
